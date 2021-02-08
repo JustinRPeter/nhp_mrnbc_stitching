423887a44e50bbeafd6a1a5417910f57bba510fd
Vi Co Duong <vico.duong@bom.gov.au>
2021-02-08 11:01:52 +1100

    FEAT: submitter.sh also submits jobs starting in current dir

--- qlogs/submitter.sh
+++ qlogs/submitter.sh
@@ -12,6 +12,7 @@ cat >> temp_submission_script <<EOF
 #PBS -P er4
 #PBS -l ncpus=26
 #PBS -l mem=1440gb
+#PBS -l wd
 
 gcm=CNRM-CM5
 rcp=rcp45
@@ -20,7 +21,8 @@ flag=awap
 
 source /g/data/er4/jr6311/miniconda/bin/activate isimip
 
-python3 /g/data/er4/jr6311/unsw-bias-correction/awap_stitch.py ${start[$ii]} ${end[$ii]}
+cd ../
+python3 awap_stitch.py ${start[$ii]} ${end[$ii]}
 
 EOF
 
