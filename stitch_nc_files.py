3478091f9cd1d28d2a1403563a527c5b040326c3
Vi Co Duong <vico.duong@bom.gov.au>
2021-02-15 19:07:01 +1100

    FIX: fix syntax

--- stitch_nc_files.py
+++ stitch_nc_files.py
@@ -9,8 +9,8 @@ def get_args():
     parser = argparse.ArgumentParser(description='Supply arguments to create subset of stitched netCDF files.')
     parser.add_argument("--gcm", required=True, help="Specify the model. e.g. ACCESS1-0")
     parser.add_argument("--rcp", required=True, help="Specify the scenario. e.g. rcp45")
-    parser.add_argument("--input_base_dir", required=True, help"The base directory containing files to stitch. Subfolder for gcm and rcp is assumed.")
-    parser.add_argument("--output_base_dir", required=True, help"The base directory to write stitched files to.")
+    parser.add_argument("--input_base_dir", required=True, help="The base directory containing files to stitch. Subfolder for gcm and rcp is assumed.")
+    parser.add_argument("--output_base_dir", required=True, help="The base directory to write stitched files to.")
 
     args = parser.parse_args()
     return args
